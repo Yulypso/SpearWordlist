1b4ddb3ce680507d749a2e105362ef9625f4e8a3
Yulypso <khamphousone.thierry@outlook.fr>
2021-10-21 19:32:25 +0200

    + regex

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import urllib.request
+import re
+from collections import Counter
 
 
-# Press the green button in the gutter to run the script.
+class Riwords:
+
+    def __init__(self, url):
+        self.url = url
+        self.webpage = self.read()
+        self.count = self.occurs()
+
+    def ignore_html(self, w):
+        return re.sub("[\(\)]?", "",
+            re.sub("^(?!href=\"|js|images)((.)*\")", "",
+            re.sub("<(.)*$", "",
+            re.sub("(.)*\">", "",
+            re.sub("<(.)*>", "",
+            re.sub("^style=\"\">", "",
+            re.sub("(<(.)*/>)", "",
+            re.sub("^(<)(!--)?(.)*(--)?(>)$", "", w)
+        )))))))
+
+    def occurs(self):
+        return Counter(w for w in self.webpage)
+
+    def read(self):
+        with urllib.request.urlopen(self.url) as url:
+            return set(self.ignore_html(w.decode("utf-8")) for l in url for w in l.split())
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    riwords = Riwords(
+        "https://fr.wikipedia.org/wiki/Commissariat_%C3%A0_l%27%C3%A9nergie_atomique_et_aux_%C3%A9nergies_alternatives")
+    print(riwords.count)
