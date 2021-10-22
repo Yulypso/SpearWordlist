10b75127555e61928a6f373e66734e7113c8f947
Yulypso <khamphousone.thierry@outlook.fr>
2021-10-23 00:25:03 +0200

    + project

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class Riwords:
                           "wp-includes", "paie", "https://de", "https://eu", "https://it", "https://nl", "https://pt",
                           "https://sv", "https://pl", "https://tr", "https://mx", "https://it", "https://us", "https://cn",
                           "https://br", "https://au", "https://ftr", "https://ko", "https://ru", "https://en", "https://uk",
-                          "https://zh", "https://bg", "https://ec", "mediawiki.org", "visiatome", "britannica"]
+                          "https://zh", "https://bg", "https://ec", "mediawiki.org", "visiatome", "britannica", "crossref"]
 
         self.filter_url_list()
 
@@ -41,7 +41,8 @@ class Riwords:
                                                                                                                       re.sub("(((>)?<.*)?(>)?(/>)?(\[)?(])?(\()?(\))?)(\|)?((.)*:)?(·)?({)?(\\)(})?)?", "",
                                                                                                                           re.sub("^(?!href=\")((.)*\")", "",
                                                                                                                               re.sub("(<!?-?-?.+-?-?>)", "",
-                                                                                                                                     re.sub("class=\"(.)*\"", "", w)))))))))))))))))))))
+                                                                                                                                     re.sub("class=\"(.)+\"", "",
+                                                                                                                                            re.sub("style=\"(.)+\"", "", w))))))))))))))))))))))
 
         if "http" not in w:
             return w
@@ -83,7 +84,7 @@ class Riwords:
     def read(self):
         req = urllib.request.Request(self.url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'})
         try:
-            with urllib.request.urlopen(req, timeout=10) as url:
+            with urllib.request.urlopen(req, timeout=3) as url:
                 return Counter(self.ignore_html(urllib.parse.unquote_plus(w.decode("utf-8"))) for l in url for w in l.split())
         except:
             print("Error: timout")
@@ -105,21 +106,20 @@ class Riwords:
         cpt = horizontal
         i = 0
 
-        total_time = time.time()
-        while total_time and deep and i < len(self.next_url_list) <= cpt:
-            total_time = 0 if time.time() - total_time > 5000.0 else total_time + (time.time() - total_time)
-            print(time.time() - total_time)
+        while deep and i < len(self.next_url_list) <= cpt:
+
             if re.match("(^https://)([a-zA-Z0-9./_%\'-])*", self.next_url_list[i]):
                 try:
-                    print(f"Searching [{i}][{deep}]: " + self.next_url_list[i])
+                    print(f"Searching [{deep}][{i}]: " + self.next_url_list[i])
                     r = Riwords(self.next_url_list[i])
                     r.parse_internal_url(len(r.next_url_list), deep-1)
-                    print(f"Add words to dict [{i}][{deep}]: " + ", ".join(r.dict_counter))
+                    print(f"Add words to dict [{deep}][{i}]: " + ", ".join(r.dict_counter))
                     self.dict_counter += r.dict_counter
-                    print(f"interesting url found [{i}][{deep}]: [" + ", ".join(r.next_url_list) + "]\n")
+                    print(f"interesting url found [{deep}][{i}]: [" + ", ".join(r.next_url_list) + "]\n")
                     i += 1
+
                 except:
-                    print(f"Error [{i}][{deep}]: Can't access url\n")
+                    print(f"Error [{deep}][{i}]: Can't access url\n")
                     cpt += 1
                     i += 1
             else:
@@ -129,5 +129,5 @@ class Riwords:
 if __name__ == '__main__':
     riwords = Riwords(
         "https://fr.wikipedia.org/wiki/Commissariat_%C3%A0_l%27%C3%A9nergie_atomique_et_aux_%C3%A9nergies_alternatives")
-    riwords.parse_internal_url(deep=1)  # horizontal=default | deep=2
+    riwords.parse_internal_url(deep=3)  # horizontal=default | deep=2
     riwords.write()
